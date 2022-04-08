35cdf2f4ad6dac63e8b547cc35e1614427ddb478
Rolake Olonisakin <rollyloni@gmail.com>
2022-04-08 07:55:31 -0700

    update feedback from sprint 2 and setup get and psot request with axios from WEB API

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,96 +1,78 @@
-const showsArray = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-  },
-  {
-    date: "Mon Sept 21 2021",
-    venue: "Pier 3 East",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-  },
-];
+const showsData = axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=b24f35ee-ae2e-4b80-bfb0-6b4836edbf96"
+  )
+  .then((response) => {
+    const showsArray = response.data;
+    console.log(response.data);
 
-const ticketEl = document.querySelector(".ticket-section");
-const ticketDivEl = document.createElement("article");
+    const ticketEl = document.querySelector(".ticket-section");
+    const ticketDivEl = document.createElement("article");
 
-const ticketHeaderEl = document.createElement("h1");
-ticketHeaderEl.classList.add("ticket-section__heading");
-ticketHeaderEl.innerText = "Shows";
-ticketEl.appendChild(ticketHeaderEl);
+    const ticketHeaderEl = document.createElement("h1");
+    ticketHeaderEl.classList.add("ticket-section__heading");
+    ticketHeaderEl.innerText = "Shows";
+    ticketEl.appendChild(ticketHeaderEl);
 
-//Created a hidden section for media Query
-const newDateTitleEl = document.createElement("p");
-newDateTitleEl.classList.add("ticket-section__title", "show", "date");
-newDateTitleEl.innerText = "DATE";
-ticketEl.appendChild(newDateTitleEl);
+    //Created a hidden section for media Query
+    const newDateTitleEl = document.createElement("p");
+    newDateTitleEl.classList.add("ticket-section__title", "show", "date");
+    newDateTitleEl.innerText = "DATE";
+    ticketEl.appendChild(newDateTitleEl);
 
-const venueTitleEl = document.createElement("p");
-venueTitleEl.classList.add("ticket-section__title", "show", "venue");
-venueTitleEl.innerText = "VENUE";
-ticketEl.appendChild(venueTitleEl);
+    const venueTitleEl = document.createElement("p");
+    venueTitleEl.classList.add("ticket-section__title", "show", "venue");
+    venueTitleEl.innerText = "VENUE";
+    ticketEl.appendChild(venueTitleEl);
 
-const locationTitleEl = document.createElement("p");
-locationTitleEl.classList.add("ticket-section__title", "show");
-locationTitleEl.innerText = "LOCATION";
-ticketEl.appendChild(locationTitleEl);
+    const locationTitleEl = document.createElement("p");
+    locationTitleEl.classList.add("ticket-section__title", "show");
+    locationTitleEl.innerText = "LOCATION";
+    ticketEl.appendChild(locationTitleEl);
 
-//Funtion to create each ticket card
-function createTicket(ticket) {
-  const ticketDivEl = document.createElement("article");
-  ticketDivEl.classList.add("ticket-section__container");
-  ticketEl.appendChild(ticketDivEl);
+    //Funtion to create each ticket card
+    function createTicket(ticket) {
+      const ticketDivEl = document.createElement("article");
+      ticketDivEl.classList.add("ticket-section__container");
+      ticketEl.appendChild(ticketDivEl);
 
-  const dateTitleEl = document.createElement("p");
-  dateTitleEl.classList.add("ticket-section__title");
-  dateTitleEl.innerText = "DATE";
-  ticketDivEl.appendChild(dateTitleEl);
+      const dateTitleEl = document.createElement("p");
+      dateTitleEl.classList.add("ticket-section__title");
+      dateTitleEl.innerText = "DATE";
+      ticketDivEl.appendChild(dateTitleEl);
 
-  const dateEl = document.createElement("p");
-  dateEl.classList.add("ticket-section__date");
-  dateEl.innerText = ticket.date;
-  ticketDivEl.appendChild(dateEl);
+      const dateEl = document.createElement("p");
+      dateEl.classList.add("ticket-section__date");
+      dateEl.innerText = ticket.date;
+      ticketDivEl.appendChild(dateEl);
 
-  const venueTitleEl = document.createElement("p");
-  venueTitleEl.classList.add("ticket-section__title");
-  venueTitleEl.innerText = "VENUE";
-  ticketDivEl.appendChild(venueTitleEl);
+      const venueTitleEl = document.createElement("p");
+      venueTitleEl.classList.add("ticket-section__title");
+      venueTitleEl.innerText = "VENUE";
+      ticketDivEl.appendChild(venueTitleEl);
 
-  const venueEl = document.createElement("p");
-  venueEl.classList.add("ticket-section__venue");
-  venueEl.innerText = ticket.venue;
-  ticketDivEl.appendChild(venueEl);
+      const venueEl = document.createElement("p");
+      venueEl.classList.add("ticket-section__venue");
+      venueEl.innerText = ticket.place;
+      ticketDivEl.appendChild(venueEl);
 
-  const locationTitleEl = document.createElement("p");
-  locationTitleEl.classList.add("ticket-section__title");
-  locationTitleEl.innerText = "LOCATION";
-  ticketDivEl.appendChild(locationTitleEl);
+      const locationTitleEl = document.createElement("p");
+      locationTitleEl.classList.add("ticket-section__title");
+      locationTitleEl.innerText = "LOCATION";
+      ticketDivEl.appendChild(locationTitleEl);
 
-  const locationEl = document.createElement("p");
-  locationEl.classList.add("ticket-section__location");
-  locationEl.innerText = "San Francisco, CA";
-  ticketDivEl.appendChild(locationEl);
+      const locationEl = document.createElement("p");
+      locationEl.classList.add("ticket-section__location");
+      locationEl.innerText = ticket.location;
+      ticketDivEl.appendChild(locationEl);
 
-  const buttonEl = document.createElement("button");
-  buttonEl.classList.add("ticket-section__button");
-  buttonEl.innerText = "BUY TICKETS";
-  ticketDivEl.appendChild(buttonEl);
-}
+      const buttonEl = document.createElement("button");
+      buttonEl.classList.add("ticket-section__button");
+      buttonEl.innerText = "BUY TICKETS";
+      ticketDivEl.appendChild(buttonEl);
+    }
 
-for (let i = 0; i < showsArray.length; i++) {
-  createTicket(showsArray[i]);
-}
+    for (let i = 0; i < showsArray.length; i++) {
+      createTicket(showsArray[i]);
+    }
+  });
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,130 +1,109 @@
-const commentBox = [
-  {
-    image: "./assets/images/photo-gallery-1.jpg",
-    name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
+const commentData = axios
+  .get(
+    "https://project-1-api.herokuapp.com/comments?api_key=b24f35ee-ae2e-4b80-bfb0-6b4836edbf96"
+  )
+  .then((response) => {
+    console.log(response.data);
 
-  {
-    image: "./assets/images/photo-gallery-2.jpg",
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
+    const commentBox = [
+      {
+        image: "./assets/images/photo-gallery-1.jpg",
+        name: response.data[0].name,
+        date: response.data[0].timestamp,
+        comment: response.data[0].comment,
+      },
+      {
+        image: "./assets/images/photo-gallery-1.jpg",
+        name: response.data[1].name,
+        date: response.data[1].timestamp,
+        comment: response.data[1].comment,
+      },
+      {
+        image: "./assets/images/photo-gallery-1.jpg",
+        name: response.data[2].name,
+        date: response.data[2].timestamp,
+        comment: response.data[2].comment,
+      },
+    ];
+    console.log(commentBox);
 
-  {
-    image: "./assets/images/photo-gallery-3.jpg",
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
-//Get all elements needed
+    //Getting Elements
+    const sectionEl = document.querySelector(".comment-section");
+    const formEl = document.querySelector(".form__name");
+    const avatarEl = document.querySelector(".comment-section__avatar");
+    const inputNameEl = document.querySelector(".form__name");
+    const inputEl = document.querySelector("fullName");
+    const inputCommentEl = document.querySelector(".form__comment");
+    const typeCommentEl = document.querySelector("comment");
+    const buttonEl = document.querySelector(".form__button");
 
-const sectionEl = document.querySelector(".comment-section");
-const formEl = document.querySelector(".form__name");
-const avatarEl = document.querySelector(".comment-section__avatar");
-const inputNameEl = document.querySelector(".form__name");
-const inputEl = document.querySelector("fullName");
-const inputCommentEl = document.querySelector(".form__comment");
-const typeCommentEl = document.querySelector("comment");
-const buttonEl = document.querySelector(".form__button");
+    //function to create comment
+    const containerEl = document.createElement("article");
+    containerEl.classList.add("comment-section__container");
+    sectionEl.appendChild(containerEl);
 
-//Would be a function
+    function displayComment(person) {
+      const divEl = document.createElement("div");
+      divEl.classList.add("comment-section__div");
+      containerEl.appendChild(divEl);
 
-//Function to create a new comment
-const containerEl = document.createElement("article");
-containerEl.classList.add("comment-section__container");
-sectionEl.appendChild(containerEl);
+      const avatarEl = document.createElement("img");
+      avatarEl.classList.add("comment-section__avatar");
+      avatarEl.setAttribute("name", "avatar");
+      avatarEl.setAttribute("src", "./assets/images/photo-gallery-1.jpg");
+      divEl.appendChild(avatarEl);
 
-function displayComment(person) {
-  const divEl = document.createElement("div");
-  divEl.classList.add("comment-section__div");
-  containerEl.appendChild(divEl);
+      const nameDivEl = document.createElement("div");
+      nameDivEl.classList.add("comment-section__name--div");
+      containerEl.appendChild(nameDivEl);
 
-  const avatarEl = document.createElement("img");
-  avatarEl.classList.add("comment-section__avatar");
-  avatarEl.setAttribute("name", "avatar");
-  avatarEl.setAttribute("src", person.image);
-  divEl.appendChild(avatarEl);
+      const nameEl = document.createElement("p");
+      nameEl.classList.add("comment-section__name");
+      nameEl.innerText = person.name;
+      divEl.appendChild(nameEl);
 
-  const nameDivEl = document.createElement("div");
-  nameDivEl.classList.add("comment-section__name--div");
-  containerEl.appendChild(nameDivEl);
+      const dateEl = document.createElement("p");
+      dateEl.classList.add("comment-section__date");
+      dateEl.innerText = person.timestamp;
+      divEl.appendChild(dateEl);
 
-  const nameEl = document.createElement("p");
-  nameEl.classList.add("comment-section__name");
-  nameEl.innerText = person.name;
-  divEl.appendChild(nameEl);
+      const textEl = document.createElement("p");
+      textEl.classList.add("comment-section__text");
+      textEl.innerText = person.comment;
+      containerEl.appendChild(textEl);
+    }
 
-  const dateEl = document.createElement("p");
-  dateEl.classList.add("comment-section__date");
-  dateEl.innerText = person.date;
-  divEl.appendChild(dateEl);
-
-  const textEl = document.createElement("p");
-  textEl.classList.add("comment-section__text");
-  textEl.innerText = person.comment;
-  containerEl.appendChild(textEl);
-}
-
-//Loop for generationg comments
-
-for (let i = 0; i < commentBox.length; i++) {
-  displayComment(commentBox[i]);
-}
-
-// Event listener for form submission
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const formEl = event.target;
-
-  const nameInput = event.target.fullName.value;
-
-  const imgInput = "./assets/images/mohan-muruge.jpg";
-
-  const typeCommentInput = event.target.comment.value;
-
-  //Error state
-
-  if (nameInput === "" || typeCommentEl === "") {
-    inputEl.style.borderColor = "red";
-    typeCommentEl.style.borderColor = "red";
-    alert("Please complete all required fields");
-    return;
-  }
-
-  //New comment creator that pushes comment unto the screen
-  const freshDate = new Date();
-  commentBox.push({
-    image: imgInput,
-    name: nameInput,
-    date:
-      freshDate.getMonth() +
-      1 +
-      "/" +
-      freshDate.getDate() +
-      "/" +
-      freshDate.getFullYear(),
-    comment: typeCommentInput,
-  });
-
-  //Created a loop that clears the default comments after submission
-  //The second loop re-renders the comments without the most recent one
-  for (let i = 0; i < commentBox.length; i++) {
-    containerEl.innerHTML = "";
-    displayComment(commentBox[i]);
-
-    for (let i = 0; i < commentBox.length - 1; i++) {
+    for (let i = 0; i < commentBox.length; i++) {
       displayComment(commentBox[i]);
     }
-  }
-  event.target.reset();
-});
 
-// Axios section
+    // Event listener for form submission
+    formEl.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const test = axios.post(
+        "https://project-1-api.herokuapp.com/comments?api_key=b24f35ee-ae2e-4b80-bfb0-6b4836edbf96",
+        {
+          name: event.target.fullName.value,
+          comment: event.target.comment.value,
+        }
+      );
+
+      //new comment
+
+      event.target.reset();
+      const postArray = response.data;
+      for (let i = 3; i < postArray.length + 1; i++) {
+        displayComment(postArray[i]);
+      }
+    });
+  });
+
+//Error state
+
+// if (nameInput === "" || typeCommentEl === "") {
+//   inputEl.style.borderColor = "red";
+//   typeCommentEl.style.borderColor = "red";
+//   alert("Please complete all required fields");
+//   return;
+// }
